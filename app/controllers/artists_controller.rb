34ed6ce213b374ac0b79f3aa52ab0abcf8949438
Russell Macalintal <russell.macalintal@gmail.com>
2020-04-13 05:03:54 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,11 +5,7 @@ class ArtistsController < ApplicationController
   end
 
   get '/artists/:slug' do
-    @artist = Artist.find_by_slug(:slug)
-    if @artist == nil
-      redirect '/artists'
-    else
-      erb :'/artists/show'
-    end
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
   end
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -8,8 +8,7 @@ module Slugifiable
 
   module ClassMethods
     def find_by_slug(slug)
-      result = nil
-      result = self.all.find { |i| i.slug == slug }
+      self.all.find { |i| i.slug == slug }
     end
   end
 end
